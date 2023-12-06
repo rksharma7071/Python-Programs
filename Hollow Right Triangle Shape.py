95ee18bd24212a2e7c8b39382c9643525ef47c11
Retesh Kumar Sharma <61469396+rksharma7071@users.noreply.github.com>
2023-12-06 19:11:12 +0000

    Hollow Right Triangle Shape

---
+++ Hollow Right Triangle Shape.py
@@ -0,0 +1,14 @@
+"""
+    Hollow Right Triangle Shape
+    
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+"""
+
+for i in range(6):
+    for j in range(1,i+1):
+        print('*', end=' ')
+    print()
